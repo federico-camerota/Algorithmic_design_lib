0774031ef2ab4f67dccb6450fa3591e54e869022
Federico Camerota <fedes770@gmail.com>
2019-03-11 22:38:03 +0100

    Improved organization of Matrix.hpp

--- leaqx8664/marsh/Matrix.hpp
+++ leaqx8664/marsh/Matrix.hpp
@@ -19,12 +19,17 @@
 namespace leaqx8664{
 
     namespace marsh{
+	
+	/////////////////////
+	// MATRIX CLASS
+	/////////////////////
+	
 	/**
 	 * @class Matrix
 	 *
 	 * @brief Class representing a matrix of scalars
 	 *
-	 * A matrix is a 2 dimensional arrya with scalar elements for which common mathematical operations
+	 * A matrix is a 2 dimensional array with scalar elements for which common mathematical operations
 	 * such as summation and multiplication are defined through the overloading of operators.
 	 */
 	template <typename T>
@@ -32,6 +37,19 @@ namespace leaqx8664{
 
 	    public:
 
+		/**
+		 * @class Matrix_block
+		 *
+		 * @brief A Matrix_block represents a block in a Matrix object
+		 * 
+		 * The Matrix_block class provides an easy way to work on sublocks of the matrix.
+		 */
+		class Matrix_block;
+
+		//! Alias for the dimensions of the matrix
+		using dimension = std::pair<size_t, size_t>;
+		//! Alias for matrix blocks
+		using block = Matrix_block;
 		//! Alias for scalar_type used in the matrix
 		using scalar_type = T;
 
@@ -50,6 +68,100 @@ namespace leaqx8664{
 		 *
 		 * This class allows iteration through a Matrix object by rows.
 		 */
+		class Matrix_iterator; 
+		/**
+		 * @class Matrix_const_iterator
+		 *
+		 * @brief Const iterator for the matrix class
+		 *
+		 * This class allows iteration through a const Matrix object by rows.
+		 */
+		class Matrix_const_iterator; 
+
+	    public:
+
+		////////////////////
+		// ALIASES FOR ITERATORS
+		////////////////////
+
+		//! Alias for matrix iterators
+		using iterator = Matrix_iterator;
+		//! Alias for matrix const iterators
+		using const_iterator = Matrix_const_iterator;
+
+
+		///////////////////
+		// MATRIX CLASS CONSTRUCTORS 
+		///////////////////
+
+		/**
+		 * Create a matrix of uninitialized values with dimension
+		 * (n_rows, n_columns)
+		 *
+		 * @param n_rows Number of rows in the matrix
+		 * @param n_columns Number of columns in the matrix
+		 */
+		Matrix (const size_t n_rows, const size_t n_columns) :
+		    dims{n_rows, n_columns}, max_index{n_rows*n_columns - 1}, elements{new T[n_rows*n_columns + 1]}
+		{}
+		/**
+		 * Copy constructor for Matrix objects
+		 *
+		 * @param other Matrix object to copy from
+		 */
+		Matrix (const Matrix<T>& other) :
+		    dims{other.dims}, max_index{other.max_index}, elements{new T[max_index + 1]}
+		{
+		    //copy elements of the given matrix in the new one
+		    for (size_t i = 0U; i <= max_index; ++i)
+			elements[i] = other.elements[i];
+		}
+		/**
+		 * Move constructor for Matrix objects
+		 *
+		 * @param other Matrix object to move from
+		 */
+		Matrix (Matrix<T>&& other) :
+		    dims{std::move(other.dims)}, max_index{other.max_index}, elements{nullptr}
+		{
+		    std::swap(elements, other.elements);
+		}
+		/**
+		 * ADD CONSTRUCTORS USING MATRIX SUBLOCKS
+		*/
+
+		/**
+		 * @brief Descturctor for the Matrix class
+		 */
+		~Matrix(){
+		
+		    delete[] elements;
+		}
+
+
+
+		///////////////////
+		// BEGIN AND END FUNCTIONS                      <----------------------------------------- ADD COMMENTS FOR EACH FUNCTION
+		///////////////////
+		iterator begin() { return iterator{elements, max_index};}
+		iterator end() { return iterator{nullptr, 0};}
+		const_iterator begin() const { return const_iterator{elements, max_index};}
+		const_iterator end() const { return const_iterator{nullptr, 0};}
+
+		
+		///////////////////
+		// GET DIMENSION MEMBER                      <----------------------------------------- ADD COMMENTS FOR EACH FUNCTION
+		///////////////////
+		dimension get_dimensions() const {
+		
+		    return dims;
+		}
+
+	    private:
+
+		//////////////////
+		// MATRIX ITERATOR CLASS
+		//////////////////
 		class Matrix_iterator : std::iterator<std::forward_iterator_tag, T> {
 
 		    //! Pointer to the current element pointed by the iterator
@@ -129,113 +241,31 @@ namespace leaqx8664{
 			    }
 
 		};
-		/**
-		 * @class Matrix_const_iterator
-		 *
-		 * @brief Const iterator for the matrix class
-		 *
-		 * This class allows iteration through a const Matrix object by rows.
-		 */
+		//////////////////
+		// MATRIX CONST ITERATOR CLASS
+		//////////////////
 		class Matrix_const_iterator : public Matrix_iterator{
 		
-		public:
-		    using Matrix_iterator::Matrix_iterator;
+		    public:
+			using Matrix_iterator::Matrix_iterator;
 
-		    /**
-		     * @brief Operator* for the Matrix_const_iterator class
-		     *
-		     * Returns the element currently pointed by the iterator as a const
-		     * reference. If all elements have already been consumed an 
-		     * ExpiredIteratorException will be throwed.
-		     */
-		    const scalar_type& operator*(){
-		    
-			return Matrix_iterator::operator*();
-		    }
+			/**
+			 * @brief Operator* for the Matrix_const_iterator class
+			 *
+			 * Returns the element currently pointed by the iterator as a const
+			 * reference. If all elements have already been consumed an 
+			 * ExpiredIteratorException will be throwed.
+			 */
+			const scalar_type& operator*(){
+			
+			    return Matrix_iterator::operator*();
+			}
 		};
-
-	    public:
-
-		/**
-		 * @class Matrix_block
-		 *
-		 * @brief A Matrix_block represents a block in a Matrix object
-		 * 
-		 * The Matrix_block class provides an easy way to work on sublocks of the matrix.
-		 */
-		class Matrix_block;
-
-		//! Alias for the dimensions of the matrix
-		using dimension = std::pair<size_t, size_t>;
-		//! Alias for matrix iterators
-		using iterator = Matrix_iterator;
-		//! Alias for matrix const iterators
-		using const_iterator = Matrix_const_iterator;
-		//! Alias for matrix blocks
-		using block = Matrix_block;
-
-
-
-		/*
-		 *  CONSTRUCTORS
-		 */
-
-		/**
-		 * Create a matrix of uninitialized values with dimension
-		 * (n_rows, n_columns)
-		 *
-		 * @param n_rows Number of rows in the matrix
-		 * @param n_columns Number of columns in the matrix
-		 */
-		Matrix (const size_t n_rows, const size_t n_columns) :
-		    dims{n_rows, n_columns}, max_index{n_rows*n_columns - 1}, elements{new T[n_rows*n_columns + 1]}
-		{}
-		/**
-		 * Copy constructor for Matrix objects
-		 *
-		 * @param other Matrix object to copy from
-		 */
-		Matrix (const Matrix<T>& other) :
-		    dims{other.dims}, max_index{other.max_index}, elements{new T[max_index + 1]}
-		{
-		    //copy elements of the given matrix in the new one
-		    for (size_t i = 0U; i <= max_index; ++i)
-			elements[i] = other.elements[i];
-		}
-		/**
-		 * Move constructor for Matrix objects
-		 *
-		 * @param other Matrix object to move from
-		 */
-		Matrix (Matrix<T>&& other) :
-		    dims{std::move(other.dims)}, max_index{other.max_index}, elements{nullptr}
-		{
-		    std::swap(elements, other.elements);
-		}
-		/**
-		 * ADD CONSTRUCTORS USING MATRIX SUBLOCKS
-		*/
-
-		/**
-		 * @brief Descturctor for the Matrix class
-		 */
-		~Matrix(){
-		
-		    delete[] elements;
-		}
-
-
-		iterator begin() { return iterator{elements, max_index};}
-		iterator end() { return iterator{nullptr, 0};}
-		const_iterator begin() const { return const_iterator{elements, max_index};}
-		const_iterator end() const { return const_iterator{nullptr, 0};}
-		dimension get_dimensions() const {
-		
-		    return dims;
-		}
-
 	};
 	
+	////////////////
+	// OPERATOR PUT TO FOR THE MATRIX CLASS                  <---------------------------- ADD COMMENTS
+	////////////////
 	template <typename T>
 	std::ostream& operator<< ( std::ostream& os, const Matrix<T>& matrix){
 	
