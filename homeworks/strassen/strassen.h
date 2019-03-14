f18254abf60f06c93b7b64e2752cf834ad138937
Federico Camerota <fedes770@gmail.com>
2019-03-14 19:43:40 +0100

    Remved homewords directory

--- homeworks/strassen/strassen.h
+++
@@ -1,8 +0,0 @@
-#ifndef __STRASSEN__
-
-#include <stdlib.h>
-
-void strassen(float **C, float **A, float **B, const size_t n);
-void improved_strassen(float **C, float **A, float **B, const size_t n);
-
-#endif // __STRASSEN__
