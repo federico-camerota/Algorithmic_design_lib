1cc8001ec094cce1260b44c345e862874dd5c818
Federico Camerota <fedes770@gmail.com>
2019-03-15 21:19:19 +0100

    Added test prototypes for Matrix class

--- tests/marsh/Matrix_tests.cpp
+++ tests/marsh/Matrix_tests.cpp
@@ -2,31 +2,37 @@
 
 #include "leaqx8664.hpp"
 #include <iostream>
+#include <array>
 
-bool test_constructor();
-bool test_copy_constructor();
-bool test_move_constructor();
+std::array<int> test_2by4_matrix{1,2,3,4,5,6,7,8};
+
+/////////////////////
+// Test the functionality of operator<< 
+/////////////////////
+bool test_operator<<();
+////////////////////
+// Test operator== by constructing two matrices and initializing them
+// with the values in test_2by4_matrix
+////////////////////
+bool test_operator==();
+////////////////////
+// Test operator!= by constructing two matrices and initializing one
+// with the values in test_2by4_matrix and the other with the same 
+// values but in reverse order.
+////////////////////
+bool test_operator!=();
+////////////////////
+// Test all versions of operator() for a matrix built from the values
+// in test_2by4_matrix.
+////////////////////
+bool test_operator()();
 
 int main(){
 
     size_t passed = 0;
     size_t total = 0;
 
-    ++total;
-    passed += test_constructor();
-   // ++total;
-   // passed += test_copy_constructor();
-   // ++total;
-   // passed += test_move_constructor();
 }
 
 
-bool test_constructor(){
 
-    leaqx8664::marsh::Matrix<double> mat{2,2};
-    double num = 2.2;
-    for (auto& x : mat)
-	x = ++num;
-    std::cout << mat << std::endl;
-    return true;
-}
